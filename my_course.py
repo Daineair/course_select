86a678764f4798dd59b29348a028d029b5defe92
edamame9 <87178597+edamame9@users.noreply.github.com>
2023-11-28 20:55:20 +0900

    Update my_course.py

--- my_course.py
+++ my_course.py
@@ -1,12 +1,6 @@
-#import globals
-import fileinput
-import my_course
-#from add_login import User
+# D0971751
 
-with open("course_list.txt", "r", encoding="utf-8") as course_file:
-        c = course_file.read().split("\n")
-with open("student_course_list.txt", "r", encoding="utf-8") as s_course_file:
-        sc = s_course_file.read().split("\n")
+import globals
 
 class Course():
     def __init__(self, cls_id, cls_name, cls_professor, cls_type, cls_num, cls_time):
@@ -17,115 +11,209 @@ class Course():
         self.cls_num = cls_num
         self.cls_time = cls_time
    
-class StudentCourse():
+class userInfo():
     def __init__(self, uid, cid):
         self.uid = uid
         self.cid = cid
 
-# def time(t):
-#     time_mapping = {
-#         "1013": "星期一 早上8點 3節",
-#         "1063": "星期一 下午1點 3節",
-#         "3032": "星期三 早上10點 2節",
-#         "5072": "星期五 下午2點 2節",
-#         "4061": "星期四 下午1點 1節",
-#         "1022": "星期一 早上9點 2節",
-#         "3042": "星期三 早上11點 2節",
-#         "2063": "星期二 下午1點 3節"
-#     }
-#     if t in time_mapping:
-#         return time_mapping[t]
-        
-def delete(state ,uid):
-    if state == 2:
-        print("\n老師無法幫學生退選，請學生自行退選")
-    else:
-        #print("in delete_section")
+class classTime():
+    def __init__(self, day, stime, hours, day2, stime2, hours2):
+        self.day = day
+        self.stime = stime
+        self.hours = hours
+        self.day2 = day2
+        self.stime2 = stime2
+        self.hours2 = hours2
+
+def time(t):
+    time_mapping = {
+        "1013": "星期一 早上8點 3節",
+        "1063": "星期一 下午1點 3節",
+        "3032": "星期三 早上10點 2節",
+        "5072": "星期五 下午2點 2節",
+        "4061": "星期四 下午1點 1節",
+        "1022": "星期一 早上9點 2節",
+        "3042": "星期三 早上11點 2節",
+        "2063": "星期二 下午1點 3節"
+    }
+    if t in time_mapping:
+        return time_mapping[t]
+
+def day(t):
+    day_mapping = {
+        "星期一": 1,
+        "星期二": 2,
+        "星期三": 3,
+        "星期四": 4,
+        "星期五": 5,
+    }
+    if t in day_mapping:
+        return day_mapping[t]
+
+global UserId
+global States
+
+def myCourses(state,uid):
+
+    course_list_path = "C:/course_select/course_list.txt"
+    CourseList = open(course_list_path, "r+", encoding="utf-8")
+    student_course_list_path = "C:/course_select/student_course_list.txt"
+    StudentCourseList = open(student_course_list_path, "r+", encoding="utf-8")
+    User_list_path = "C:/course_select/User_list.txt"
+    UserList = open(User_list_path, "r+", encoding="utf-8")
+
+    myCoursesList = []
+    timetable = [[0] * 5 for i in range(15)]
+
+    C_list = CourseList.read().split("\n")
+    S_list = StudentCourseList.read().split("\n")
+    U_list = UserList.read().split("\n")
+
+
+    cnt=0
+    if (state == 1):
         studentID = uid
-        while 1==1:
-            #print("in choose")
-            choose=0
-            choose=input("選擇退選代號")
-            choose_flag=0
-            for k in range(len(sc)): #對身分
-                num = sc[k].split()
-                studentCourse = StudentCourse(num[0], num[1])
-                if studentID==num[0]:
-                    if choose == str(studentCourse.cid):
-                        choose_flag=1
-                        print("已選擇退選"+choose)
-                        break
-            if choose_flag==1:   
-                break
-            if choose_flag==0:   
-                print("您的課程沒有代碼為"+choose+"，請再輸入一次")
-        minus=0
-        total=0
-        final=0
-        for j in range(len(c)): #找該課程學分
-            unit = c[j].split()
-            course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
-            if choose == unit[0]: 
-                minus=int(unit[4])
-                #print("扣該課程學分" + str(minus))
-                break
-        for i in range(len(sc)): #對身分
-            num = sc[i].split()
-            studentCourse = StudentCourse(num[0], num[1]) 
-            if studentCourse.uid == studentID:    
-                for k in range(len(c)): #對課表課程
-                    unit = c[k].split()
-                    course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
-                    if course.cls_id == studentCourse.cid: 
-                        total+=int(unit[4])
-                        #print("目前課程學分" + str(total))
-        final=total-minus
-        #print("總學分" + str(final))
-        if final>=7:
-            deleteself=3
-            for i in range(len(c)): #找該課程學分
-                    #print("in loop")
-                    unit = c[i].split()
-                    course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
-                    if choose == unit[0]: 
-                        deleteself=unit[3]
-                        #print("是否為必修:"+deleteself+" unit[0]:"+unit[0]+" unit[3]:"+unit[3])
-                        break
-            #print("deleteself:"+str(deleteself))
-            if deleteself=="1":
-                # delete_id=0
-                # delete_cid=0
-                # for i in range(len(sc)): #對身分
-                #     num = sc[i].split()
-                #     studentCourse = StudentCourse(num[0], num[1])
-                #     if choose==num[1]:
-                #         delete_id=num[0]
-                #         delete_cid=num[1]
-                #         print()
-                #         break
-                print("delete_id:"+studentID+" delete_cid:"+choose)
-                with fileinput.FileInput("student_course_list.txt", inplace=True, backup=".bak") as file:
-                   for line in file:
-                       if f"{studentID}\t{choose}" not in line:
-                           print(line, end='')
-                print("退選成功，以下是您的課表")
-                course_file.close()
-                s_course_file.close()
-                my_course.myCourses(state,uid)
-                # with open("C:/course_select/student_course_list.txt", "r", encoding="utf-8") as s_course_file:
-                #     sc2 = s_course_file.read().split("\n")
-                # for i in range(len(sc2)): #對身分
-                #     num2 = sc2[i].split()
-                #     studentCourse2 = StudentCourse(num2[0], num2[1]) 
-                #     if studentCourse2.uid == studentID:
-                #         for j in range(len(c)): #對課表課程
-                #             unit = c[j].split()
-                #             course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
-                #             if course.cls_id == studentCourse2.cid: 
-                #                print(f'課程代碼: {course.cls_id} 課程名稱: {course.cls_name} 課程學分: {course.cls_num} 課程時間: {time(course.cls_time)}')
+        for i  in range(len(U_list)):
+            UserID = U_list[i].split()
+            if(UserID[0] == studentID):
+                student_info = userInfo(UserID[0],UserID[2])
+        for i in range(len(S_list)):
+            SID = S_list[i].split()
+            if(len(SID)>1):
+                if(SID[0] == studentID):
+                    CourseID = SID[1]
+                    for j in range(len(C_list)):
+                        CID = C_list[j].split()
+                        course = Course(CID[0], CID[1], CID[2], CID[3], CID[4], CID[5])
+                        if(CourseID == course.cls_id):
+                            myCoursesList.append(course)
+                            Class_time = time(course.cls_time).split()
+                            if(len(Class_time)<4):
+                                class_day = int (course.cls_time[0])
+                                class_time = int (course.cls_time[2])
+                                class_num = int (course.cls_time[3])
+                                for i in range(class_num):
+                                    timetable[class_time+i-1][class_day-1] = course.cls_name
+                                break
+                            else:
+                                t = classTime(Class_time[0],Class_time[1],Class_time[2],Class_time[4],Class_time[5],Class_time[6])
+                                class_day = day(t.day)
+                                if(len(t.stime)==4):
+                                    if(t.stime in "早上"):
+                                        class_time = int (t.stime[2]) - 7
+                                    else:
+                                        class_time = int (t.stime[2]) + 5
+                                else:
+                                    if(t.stime in "早上"):
+                                        class_time = int (t.stime[2]+t.stime[3]) - 7
+                                    else:
+                                        class_time = int (t.stime[2]+t.stime[3]) + 5
+                                
+                                class_num = int (t.hours[0])
+                                for i in range(class_num):
+                                    timetable[class_time+i-1][class_day-1] = course.cls_name
+                                class_day = day(t.day2)
+                                if(len(t.stime2)==4):
+                                    if(t.stime2 in "早上"):
+                                        class_time = int (t.stime2[2]) - 7
+                                    else:
+                                        class_time = int (t.stime2[2]) + 5
+                                else:
+                                    if(t.stime in "早上"):
+                                        class_time = int (t.stime2[2]+t.stime2[3]) - 7
+                                    else:
+                                        class_time = int (t.stime2[2]+t.stime2[3]) + 5
+                                class_num = int (t.hours2[0])
+                                for i in range(class_num):
+                                    timetable[class_time+i-1][class_day-1] = course.cls_name
+                                break
+
+        print("\n姓名: "+student_info.cid+"\t學號: "+student_info.uid+"\n")
+        print("-----------------------------------------------------------------------------------------")
+        print("|   \t|星期一\t\t|星期二\t\t|星期三\t\t|星期四\t\t|星期五\t\t|")
+
+        print("-----------------------------------------------------------------------------------------")
+        for i in range(15):
+            print("|",i+1,"\t"+"|",end="")
+            for j in range(5):
+                if(timetable[i][j]!=0):
+                    print(timetable[i][j],"\t"+"|",end="")
+                else:
+                    print("\t\t"+"|",end="")
+            print("")
+            print("-----------------------------------------------------------------------------------------")
+    else:
+        teacherID = uid
+        for i  in range(len(U_list)):
+            UserID = U_list[i].split()
+            if(UserID[0] == teacherID):
+                teacher_info = userInfo(UserID[0],UserID[2])
+        for i in range(len(C_list)):
+            CID = C_list[i].split()
+            if(len(CID)>4):
+                if(CID[2] == teacherID):
+                    CourseID = CID[0]
+                    course = Course(CID[0], CID[1], CID[2], CID[3], CID[4], CID[5])
+                    if(CourseID == course.cls_id):
+                        cnt += cnt + 1
+                        myCoursesList.append(course)
+                        class_day = int (course.cls_time[0])
+                        class_time = int (course.cls_time[2])
+                        class_num = int (course.cls_time[3])
+                        for j in range(class_num):
+                            timetable[class_time+j-1][class_day-1] = course.cls_name
+            
+        print("\n姓名: "+teacher_info.cid+"\t學號: "+teacher_info.uid+"\n")
+        print("-----------------------------------------------------------------------------------------")
+        print("|   \t|星期一\t\t|星期二\t\t|星期三\t\t|星期四\t\t|星期五\t\t|")
+
+        print("-----------------------------------------------------------------------------------------")
+        for i in range(15):
+            print("|",i+1,"\t"+"|",end="")
+            for j in range(5):
+                if(timetable[i][j]!=0):
+                    print(timetable[i][j],"\t"+"|",end="")
+                else:
+                    print("\t\t"+"|",end="")
+            print("\n-----------------------------------------------------------------------------------------\n")
+    if(state == 1):
+        print(f'學號: {studentID}的課表\n')
+    else:
+        print(f'學號: {teacherID}的課表\n')
+    for i in range(len(S_list)): #對身分
+        SID = S_list[i].split()
+        if(len(SID)>1):
+            if(state == 1):
+                if SID[0] == studentID:
+                    for j in range(len(C_list)): #對課表課程
+                        unit = C_list[j].split()
+                        # print(unit)
+                        if(len(unit)>4):
+                            course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
+                            if course.cls_id == SID[1]: 
+                                print(f'課程代碼: {course.cls_id} 課程名稱: {course.cls_name} 課程學分: {course.cls_num} 課程時間: {time(course.cls_time)}')
+                
             else:
-                print("退選失敗，必修不能退選")
-        else:
-            print("退選失敗，學分下限不能低於七學分")
-course_file.close()
-s_course_file.close()
+                if SID[0] == teacherID:
+                    for j in range(len(C_list)): #對課表課程
+                        unit = C_list[j].split()
+                        # print(unit)
+                        if(len(unit)>4):
+                            course = Course(unit[0], unit[1], unit[2], unit[3], unit[4], unit[5])
+                            if course.cls_id == SID[1]: 
+                                print(f'課程代碼: {course.cls_id} 課程名稱: {course.cls_name} 課程學分: {course.cls_num} 課程時間: {time(course.cls_time)}')
+    credit(state,myCoursesList)
+    CourseList.close()
+    StudentCourseList.close()
+    UserList.close()
+
+
+
+def credit(state,myCoursesList):
+    sum = 0
+    if(state == 1):
+        for i in myCoursesList:
+            sum = sum + int (i.cls_num)
+        print("\n總學分：" ,sum,"\n")
+    myCoursesList[:] = []
+    sum = 0
